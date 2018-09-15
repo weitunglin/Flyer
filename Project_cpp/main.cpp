0ec749f8baeffb69d5917a45271f353379dd5cc1
weitunglin <weitung8@gmail.com>
2018-09-16 00:05:55 +0800

    Initial Commit

---
+++ Project_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_cpp
+//
+//  Created by weitung on 2018/9/16.
+//  Copyright © 2018 weitung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
